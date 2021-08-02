c7a7b485a0257a2743f767f8b1eff4132543f997
Patrick <24683548+pohuing@users.noreply.github.com>
2021-08-02 23:49:05 +0200

    Cleaned up ReaderTime

--- src/main/java/de/opentiming/feigWS/reader/ReaderTime.java
+++ src/main/java/de/opentiming/feigWS/reader/ReaderTime.java
@@ -12,6 +12,10 @@ public class ReaderTime {
 	private FedmIscReader fedm;
 	private FedmConnect con;
 
+	/**
+	 * Sets the reader time to the current system time
+	 * @return the updated reader time
+	 */
 	public synchronized String setTime() {
 		
 		String time = getComputerTime();
@@ -20,7 +24,7 @@ public class ReaderTime {
 		
 		// Durch probleme beim setzen der Zeit wenn die Sekunde kleiner 10 ist
 		// wird gewartet bis die Sekunden auf jeden Fall zweistellig sind
-		if(timeArr[2].substring(0, 1).equals("0")) {
+		if(timeArr[2].startsWith("0")) {
 			try {
 				log.info("{} sorry, warte 10 sec, bis die Sekunde zweistellig ist.");
 				Thread.sleep(10000);
@@ -72,14 +76,10 @@ public class ReaderTime {
 				fedm.sendProtocol((byte) 0x87);    // Datum und Uhrzeit setzen
 			}
 									
-		} catch (FePortDriverException e) {
+		} catch (FePortDriverException | FeReaderDriverException | FedmException e) {
 			e.printStackTrace();
-		} catch (FeReaderDriverException e) {
-			e.printStackTrace();
-		} catch (FedmException e) {
-			e.printStackTrace();
-		} 
-		
+		}
+
 		String nrTime = getReaderDateTime();
 		log.info("{} set reader Time {}", con.getHost(), nrTime.substring(0, 19));
 		returnString = nrTime.substring(0, 19);
@@ -92,30 +92,20 @@ public class ReaderTime {
 		try
 		{
 			fedm.sendProtocol((byte)0x88);
-		} catch (FePortDriverException e) {
-			e.printStackTrace();
-		} catch (FeReaderDriverException e) {
-			e.printStackTrace();
-		} catch (FedmException e) {
+		} catch (FePortDriverException | FeReaderDriverException | FedmException e) {
 			e.printStackTrace();
 		}
-		
+
 		int stunde = fedm.getIntegerData(FedmIscReaderID.FEDM_ISC_TMP_DATE_HOUR);
-		String minute = Integer.toString(fedm.getIntegerData(FedmIscReaderID.FEDM_ISC_TMP_DATE_MINUTE));
-		String sekunde = Integer.toString(fedm.getIntegerData(FedmIscReaderID.FEDM_ISC_TMP_DATE_MILLISECOND));
-		
-		while(minute.length() < 2) {
-			minute = "0" + minute;
-		}
-		
-		while(sekunde.length() < 5) {
-			sekunde = "0" + sekunde;
-		}
-		
-		String time = stunde + ":" + minute + ":" + sekunde;
-		return(time);
+		String minute = String.format("%02d", fedm.getIntegerData(FedmIscReaderID.FEDM_ISC_TMP_DATE_MINUTE));
+		String millisekunde = String.format("%05d", fedm.getIntegerData(FedmIscReaderID.FEDM_ISC_TMP_DATE_MILLISECOND));
+
+		return stunde + ":" + minute + ":" + millisekunde;
 	}
 
+	/**
+	 * @return Returns the current reader time and date in yyyy-MM-dd hh:mm:SSSSS(that's 5 digit milliseconds)
+	 */
 	public String getReaderDateTime() {
 
 		int century = 0;
@@ -124,7 +114,7 @@ public class ReaderTime {
 		int day     = 0;
 		int std     = 0;
 		int min     = 0;
-		int sek     = 0;
+		int milli     = 0;
 		String date = "";
 		
 		try
@@ -134,7 +124,7 @@ public class ReaderTime {
 				fedm.sendProtocol((byte) 0x86);
 				std = fedm.getIntegerData(FedmIscReaderID.FEDM_ISCLR_TMP_TIME_H);
 				min = fedm.getIntegerData(FedmIscReaderID.FEDM_ISCLR_TMP_TIME_M);
-				sek = fedm.getIntegerData(FedmIscReaderID.FEDM_ISCLR_TMP_TIME_MS);
+				milli = fedm.getIntegerData(FedmIscReaderID.FEDM_ISCLR_TMP_TIME_MS);
 				date = getComputerDate();
 			} else {
 				fedm.sendProtocol((byte) 0x88);
@@ -144,44 +134,35 @@ public class ReaderTime {
 				day = fedm.getIntegerData(FedmIscReaderID.FEDM_ISC_TMP_DATE_DAY);
 				std = fedm.getIntegerData(FedmIscReaderID.FEDM_ISC_TMP_DATE_HOUR);
 				min = fedm.getIntegerData(FedmIscReaderID.FEDM_ISC_TMP_DATE_MINUTE);
-				sek = fedm.getIntegerData(FedmIscReaderID.FEDM_ISC_TMP_DATE_MILLISECOND);
+				milli = fedm.getIntegerData(FedmIscReaderID.FEDM_ISC_TMP_DATE_MILLISECOND);
 				date = century + "" + year + "-" + month + "-" + day;
 			}
 			
-		} catch (FePortDriverException e) {
+		} catch (FePortDriverException | FeReaderDriverException | FedmException e) {
 			e.printStackTrace();
-		} catch (FeReaderDriverException e) {
-			e.printStackTrace();
-		} catch (FedmException e) {
-			e.printStackTrace();
-		}
-				
-
-		String stunde = Integer.toString(std);
-		while(stunde.length() < 2) {
-			stunde = "0" + stunde;
-		}
-
-		String minute = Integer.toString(min);
-		while(minute.length() < 2) {
-			minute = "0" + minute;
 		}
 		
-		String sekunde = Integer.toString(sek);
-		while(sekunde.length() < 5) {
-			sekunde = "0" + sekunde;
-		}
-		
-		String dateTime = date + " " + stunde + ":" + minute + ":" + sekunde;
-		return(dateTime);
+		String stunde = String.format("%02d", std);
+		String minute = String.format("%02d", min);
+		String millisekunde = String.format("%05d", milli);
+
+		// TODO: 02.08.2021 Since every call of getReaderTime truncates the milliseconds to seconds, maybe we should
+		//  just only return seconds
+		return date + " " + stunde + ":" + minute + ":" + millisekunde;
 	}
-	
+
+	/**
+	 * @return The current system date in yyyy-MM-dd
+	 */
 	public static String getComputerDate() {
 		Date now = new java.util.Date();
 		java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd");
 		return sdf.format(now);
 	}
-	
+
+	/**
+	 * @return The current system time in HH:mm:ss
+	 */
 	public String getComputerTime() {
 		Date now = new java.util.Date();
 		java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("HH:mm:ss");
